250d1c8a04366abf1f848dafda6d883f7de438c1
ssoup0224 <110037447+ssoup0224@users.noreply.github.com>
2022-11-11 09:22:53 +0900

    Update SVCD.py

--- change_detection_pytorch/datasets/SVCD.py
+++ change_detection_pytorch/datasets/SVCD.py
@@ -8,9 +8,9 @@ from change_detection_pytorch.datasets.custom import CustomDataset
 class SVCD_Dataset(CustomDataset):
     """ season-varying change detection dataset"""
 
-    def __init__(self, img_dir, sub_dir_1='A', sub_dir_2='B', ann_dir=None, img_suffix='.jpg', seg_map_suffix='.jpg',
+    def __init__(self, img_dir, sub_dir_1='A', ann_dir=None, img_suffix='.png', seg_map_suffix='.png',
                  transform=None, split=None, data_root=None, test_mode=False, size=256, debug=False):
-        super().__init__(img_dir, sub_dir_1, sub_dir_2, ann_dir, img_suffix, seg_map_suffix, transform, split,
+        super().__init__(img_dir, sub_dir_1, ann_dir, img_suffix, seg_map_suffix, transform, split,
                          data_root, test_mode, size, debug)
 
     def get_default_transform(self):
@@ -22,7 +22,7 @@ class SVCD_Dataset(CustomDataset):
             # A.RandomRotate90(p=0.5),
             A.Normalize(),
             ToTensorV2()
-        ], additional_targets={'image_2': 'image'})
+        ], additional_targets={'image'})
         return default_transform
 
     def get_test_transform(self):
@@ -31,7 +31,7 @@ class SVCD_Dataset(CustomDataset):
         test_transform = A.Compose([
             A.Normalize(),
             ToTensorV2()
-        ], additional_targets={'image_2': 'image'})
+        ], additional_targets={'image'})
         return test_transform
 
     def __getitem__(self, idx):
@@ -45,12 +45,12 @@ class SVCD_Dataset(CustomDataset):
 
         if not self.ann_dir:
             ann = None
-            img1, img2, filename = self.prepare_img(idx)
-            transformed_data = self.transform(image=img1, image_2=img2)
-            img1, img2 = transformed_data['image'], transformed_data['image_2']
-            return img1, img2, filename
+            img1, filename = self.prepare_img(idx)
+            transformed_data = self.transform(image=img1)
+            img1 = transformed_data['image']
+            return img1, filename
         else:
-            img1, img2, ann, filename = self.prepare_img_ann(idx)
-            transformed_data = self.transform(image=img1, image_2=img2, mask=ann)
-            img1, img2, ann = transformed_data['image'], transformed_data['image_2'], transformed_data['mask']
-            return img1, img2, ann, filename
+            img1, ann, filename = self.prepare_img_ann(idx)
+            transformed_data = self.transform(image=img1, mask=ann)
+            img1, ann = transformed_data['image'], transformed_data['mask']
+            return img1, ann, filename
